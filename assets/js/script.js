99cf87de154d0f620bcd64c95af83dd5d96e4c90
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-04-09 15:34:30 -0400

    added first day of five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
+var cityNameEl = document.querySelector("#cityname");
+var currentDateEl = document.querySelector("#date");
+var currentWeatherImageEl = document.querySelector("#currentweatherimage");
+var tempEl = document.querySelector("#temp");
+var windEl = document.querySelector("#wind");
+var humidEl = document.querySelector("#humid");
+var uvEl = document.querySelector("#uv");
+
+var dailyEl = document.querySelector("#day1");
 
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -33,14 +41,14 @@ var getApi = function (cityName) {
       return response.json();
     })
     .then(function (data) {
-      console.log("Fetch Response \n-------------");
-      console.log(data);
+      // console.log("Fetch Response \n-------------");
+      // console.log(data);
       var lat = data[0].lat;
       var lon = data[0].lon;
-      console.log(lat);
-      console.log(lon);
+      // console.log(lat);
+      // console.log(lon);
 
-    //   This is the api call to get the weather and the 5 day forecast using the lat/lon above
+      //   This is the api call to get the weather and the 5 day forecast using the lat/lon above
       var requestWeather =
         "https://api.openweathermap.org/data/2.5/onecall?lat=" +
         lat +
@@ -56,6 +64,42 @@ var getApi = function (cityName) {
         .then(function (data) {
           console.log("Fetch Response \n-------------");
           console.log(data);
+          cityNameEl.textContent = cityName;
+          var date = moment().format("dddd, MMM Do YYYY");
+          currentDateEl.textContent = date;
+
+          var currentImage = data.current.weather[0].icon;
+          // var currentImageUrl = "http://openweathermap.org/img/wn/" + currentImage + "@2x.png";
+          var insertImg = document.createElement("img");
+          insertImg.src =
+            "http://openweathermap.org/img/wn/" + currentImage + "@2x.png";
+          currentWeatherImageEl.append(insertImg);
+
+          var currentTemp = data.current.temp;
+          var currentWind = data.current.wind_speed;
+          var currentHumid = data.current.humidity;
+          var currentUv = data.current.uvi;
+
+          tempEl.textContent = currentTemp;
+          windEl.textContent = currentWind;
+          humidEl.textContent = currentHumid;
+          uvEl.textContent = currentUv;
+          var dailyImageIcon = data.daily[0].weather[0].icon;
+          var dailyTemp = data.daily[0].temp.day;
+          var dailyWind = data.daily[0].wind_speed;
+          var dailyHumid = data.daily[0].humidity;
+          console.log(dailyImageIcon);
+
+          dailyEl.innerHTML = `
+          <img src = "http://openweathermap.org/img/wn/${dailyImageIcon}.png";>
+              <p>Temp: ${dailyTemp}</p>
+          <p>Wind: ${dailyWind}</p>
+          <p>Humidity: ${dailyHumid}</p>        
+          `;
+          // tempEl.textContent = currentTemp;
+          // windEl.textContent = currentWind;
+          // humidEl.textContent = currentHumid;
+          // uvEl.textContent = currentUv;
         });
     });
 };
